8d2f15df632dc4161b098757fc47c44f44dfdf45
JuampiRombola <juampi.rombola@gmail.com>
2016-09-19 11:39:42 -0300

    Finger 4
    Nueva versión de la función de hash

--- fhash.py
+++ fhash.py
@@ -1,7 +1,8 @@
 def myhash(s):
     h = 3722
+    largo = len(s)
     for c in s:
-        h += ord(c) ** 3
+        h += (ord(c) - largo/20) ** 3 
         h ^= h >> 5
     h ^= h >> 5
     return h

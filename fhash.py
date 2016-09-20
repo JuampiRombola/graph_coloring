db354527f3f4a476eb2e7c978e6d8f9dc016f8f8
JuampiRombola <juampi.rombola@gmail.com>
2016-09-19 22:47:29 -0300

    Update fhash.py

--- fhash.py
+++ fhash.py
@@ -2,7 +2,7 @@ def myhash(s):
     h = 3722
     largo = len(s)
     for c in s:
-        h += (ord(c) - largo/20) ** 3 
+        h += (ord(c) - (largo-7)/20) ** 3 
         h ^= h >> 5
     h ^= h >> 5
     return h
